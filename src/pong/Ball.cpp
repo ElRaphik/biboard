0c5e545b562d1044794e7dd2b4d976813c87718f
Quentin RA <quentin.ramsamy.cf@gmail.com>
2022-05-16 16:21:00 +0200

    aza

--- src/pong/Ball.cpp
+++ src/pong/Ball.cpp
@@ -21,7 +21,7 @@ int Ball::update(const InputManager &manager, const PongBar &bar, const PongBar 
     if(isColliding(bar) || isColliding(bar2))
         xForce = - xForce;
     else if(x+xForce == 0) rtr=2;
-    else if(x+xForce==31) rtr=1;
+    else if(x+xForce==32) rtr=1;
     update(manager);
     return rtr;
 }
